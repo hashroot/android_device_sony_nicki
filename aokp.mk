ea90eedd53a52d08b97fbb6fc81caff3b08f68b9
hashroot <rishabhad93@gmail.com>
2014-06-10 12:51:45 +0530

    nicki: suck update, much swag, wow

--- aokp.mk
+++ aokp.mk
@@ -1,27 +1,16 @@
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/aokp/configs/gsm.mk)
-
-# Inherit some common CM stuff.
+# Inherit common product files.
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/sony/nicki/nicki.mk)
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
-TARGET_SCREEN_HEIGHT := 854
-TARGET_SCREEN_WIDTH := 480
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := nicki
-PRODUCT_NAME := aokp_nicki
-PRODUCT_BRAND := Sony
-PRODUCT_MODEL := C1905
-PRODUCT_MANUFACTURER := Sony
-PRODUCT_CHARACTERISTICS := phone
+# Inherit AOSP device configuration for tsubasa
+$(call inherit-product, device/sony/nicki/full_nicki.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=C1905 \
     BUILD_FINGERPRINT="Sony/C1905/C1905:4.3/15.4.A.0.23/eng.user.20140307.104342:user/release-keys" \
     PRIVATE_BUILD_DESC="C1905-user 4.3 2.22.J.0.40 eng.user.20140307.104342 test-keys"
 
-# Release name
-PRODUCT_RELEASE_NAME := XperiaM
+#skip asserts for now
+TARGET_OTA_ASSERT_SKIP := true
